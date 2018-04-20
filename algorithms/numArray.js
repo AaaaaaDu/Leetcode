83c59424f726129a7cba6a14e9ec15e02db973e6
DcySc <congyingdu00@gmail.com>
2018-04-20 17:20:31 +0800

    303. 区域和检索 - 不可变
    给定一个数组，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。

---
+++ algorithms/numArray.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    this.nums = nums;
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+    var sum = this.nums[i];
+    while(i<j){
+        sum += this.nums[i+1];
+        i++;
+    }
+    return sum;
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = Object.create(NumArray).createNew(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
