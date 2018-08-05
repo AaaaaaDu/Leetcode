770c0aaab21b259a487c91c89d515f1d67100f24
DcySc <congyingdu00@gmail.com>
2018-08-05 09:53:10 +0800

    236. 二叉树的最近公共祖先
    给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。

---
+++ algorithms/lowestCommonAncestor.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if(!root || p===root || q===root) return root;
+    let left = lowestCommonAncestor(root.left, p, q);
+    let right = lowestCommonAncestor(root.right, p, q);
+    
+    if(left && right){
+        return root;
+    }
+    return left === null? right : left;
+};
