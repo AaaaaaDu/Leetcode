9685064b352ea2cdeac68cf1a00ba497b15101ad
DcySc <congyingdu00@gmail.com>
2018-08-08 10:04:33 +0800

    Search in a Binary Search Tree
    Search in a Binary Search Tree

---
+++ algorithms/searchBST.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    if(!root) return [];
+    
+    if(root.val > val){
+        return searchBST(root.left, val);
+    }else if(root.val < val){
+        return searchBST(root.right, val);
+    }else{
+        return root;
+    }
+};
