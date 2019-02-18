c3210c9373fec82af323e0b2cb55314e6b00e2d6
DcySc <congyingdu00@gmail.com>
2019-02-18 15:53:34 +0800

    973. 最接近原点的 K 个点
    我们有一个由平面上的点组成的列表 points。需要从中找出 K 个距离原点 (0, 0) 最近的点。
    
    （这里，平面上两点之间的距离是欧几里德距离。）
    
    你可以按任何顺序返回答案。除了点坐标的顺序之外，答案确保是唯一的。

---
+++ algorithms/kClosest.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[][]} points
+ * @param {number} K
+ * @return {number[][]}
+ */
+var kClosest = function(points, K) {
+    points.sort((a, b) => {
+        return (a[0] * a[0] + a[1] * a[1]) - (b[0] * b[0] + b[1] * b[1])
+    });
+    
+    return points.slice(0, K);
+};
