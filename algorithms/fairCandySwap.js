4e67e7e24a5de1bfb5bff9f3aacf93b7537397d9
DcySc <congyingdu00@gmail.com>
2019-03-06 14:22:06 +0800

    Update fairCandySwap.js

--- algorithms/fairCandySwap.js
+++ algorithms/fairCandySwap.js
@@ -4,13 +4,17 @@
  * @return {number[]}
  */
 var fairCandySwap = function(A, B) {
-    const a = A.reduce((sum, val) => sum + val),
-          b = B.reduce((sum, val) => sum + val),
+    const a = A.reduce(add),
+          b = B.reduce(add),
           change = -(a - b) / 2,
           len = A.length;
     
     for (let i = 0; i < len; i++) {
         if (B.includes(A[i] + change)) return [A[i], A[i] + change];
+    }
+
+    function add (sum, val)  {
+        return sum + val;
     }
 
 };
